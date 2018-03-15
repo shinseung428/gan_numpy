cab02415264b42940a0f507bf3dd363284562e13
shin <shinseung428@gmail.com>
2018-03-15 16:55:44 +0900

    update

--- activations.py
+++ activations.py
@@ -1,18 +1,48 @@
 import numpy as np
 
+
+def mnist_reader():
+	def one_hot(label, output_dim):
+		one_hot = np.zeros((len(label), output_dim))
+		
+		for idx in range(0,len(label)):
+			one_hot[idx, label[idx]] = 1
+		
+		return one_hot
+
+
+	#Training Data
+	f = open('./data/train-images.idx3-ubyte')
+	loaded = np.fromfile(file=f, dtype=np.uint8)
+	trainX = loaded[16:].reshape((60000, 28, 28, 1)).astype(np.float32) / 127.5 - 1
+
+
+	f = open('./data/train-labels.idx1-ubyte')
+	loaded = np.fromfile(file=f, dtype=np.uint8)
+	trainY = loaded[8:].reshape((60000)).astype(np.int32)
+	trainY = one_hot(trainY, 10)
+
+
+	# #Test Data
+	# f = open('./data/t10k-images.idx3-ubyte')
+	# loaded = np.fromfile(file=f, dtype=np.uint8)
+	# testX = loaded[16:].reshape((10000, 28, 28, 1)).astype(np.float32) / / 127.5 - 1
+
+	# f = open('./data/t10k-labels.idx1-ubyte')
+	# loaded = np.fromfile(file=f, dtype=np.uint8)
+	# testY = loaded[8:].reshape((10000)).astype(np.int32)
+	# testY = one_hot(testY, 10)
+
+	return trainX, trainY, len(trainX) #, testX, testY, len(testX)
+
 def sigmoid(input, derivative=False):
-	res = []
-	for batch in input:
-		# res.append(1/(1+np.exp(-batch)))
-		tmp = 1/(1+np.exp(-batch))
-		if derivative:
-			res.append(tmp*(1-tmp))
-		else:
-			res.append(tmp)
+	# for batch in input:
+	res = 1/(1+np.exp(-input))
 
-	res = np.array(res)
+	if derivative:
+		return np.clip(res*(1-res), -0.01, 0.01)
 
-	return res
+	return np.clip(res, -0.01, 0.01)
 
 def relu(input, derivative=False):
 	res = []
@@ -24,6 +54,7 @@ def relu(input, derivative=False):
 
 
 def tanh(input, derivative=False):
-	res = np.tanh(input)
+	res = np.clip(np.tanh(input), -0.001, 0.001)
 	return res
+
 
--- gan.py
+++ gan.py
@@ -35,22 +35,22 @@ class GAN(object):
 		flattened = np.reshape(img, (self.batch_size, -1))
 
 		self.h0 = np.matmul(flattened, self.d_W0) + self.d_b0
-		self.h0 = relu(self.h0)
+		# self.h0 = relu(self.h0)
 
 		self.h1 = np.matmul(self.h0, self.d_W1) + self.d_b1
-		self.h1 = relu(self.h1)
+		# self.h1 = relu(self.h1)
 
 		self.h2 = np.matmul(self.h1, self.d_W2) + self.d_b2
-		
+
 		return self.h2, sigmoid(self.h2)
 
 	def generator(self, z):
 
 		self.h0 = np.matmul(z, self.g_W0) + self.g_b0
-		self.h0 = relu(self.h0)
+		# self.h0 = relu(self.h0)
 
 		self.h1 = np.matmul(self.h0, self.g_W1) + self.g_b1
-		self.h1 = relu(self.h1)
+		# self.h1 = relu(self.h1)
 
 		self.h2 = np.matmul(self.h1, self.g_W2) + self.g_b2
 		self.h2 = tanh(self.h2)
@@ -58,22 +58,40 @@ class GAN(object):
 		self.out = np.reshape(self.h2, (self.batch_size, 28, 28))
 		
 		return self.out
-		
 
-	def forawrd(self):
-		pass
-
-	def backward(self):
-		pass
 
 	def train(self):
-
-		z = np.random.random([self.batch_size,100])
-
-		g_img = self.generator(z)
-
-		logits, output = self.discriminator(g_img)
+		#we don't need labels
+		trainX, _, train_size = mnist_reader()
 		
+		batch_idx = train_size//self.batch_size
+		for idx in range(batch_idx):
+			train_batch = trainX[idx*self.batch_size:idx*self.batch_size + self.batch_size]
+
+			z = np.random.random([self.batch_size,100]) * 2 - 1
+
+			#forward pass
+			fake_img = self.generator(z)
+			real_logits, real_output = self.discriminator(train_batch)
+			fake_logits, fake_output = self.discriminator(fake_img)
+
+			#calculate loss
+			real_label = np.ones((self.batch_size, 1))
+			fake_label = np.zeros((self.batch_size, 1))
+
+			#cross entropy loss using sigmoid output
+			real_loss = -(real_label*np.log(real_output) + (1-real_label)*np.log(1-real_output))
+			fake_loss = -(fake_label*np.log(fake_output) + (1-fake_label)*np.log(1-fake_output))
+
+			print real_output
+			
+			# print "Real:", np.sum(real_loss)/self.batch_size
+			# print "Fake:", np.sum(fake_loss)/self.batch_size
+			
+			#train discriminator
+
+
+			#train generator
 
 
 

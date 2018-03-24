28ed7d1b10b3960ba5523ddca3278969b4ab8343
Arjun Srivastava <arjunbazinga@gmail.com>
2018-03-24 18:37:54 +0530

    Made code python3 compliant

--- gan.py
+++ gan.py
@@ -28,7 +28,7 @@ class GAN(object):
 		self.g_W1 = np.random.randn(128,784).astype(np.float32) * np.sqrt(2.0/(128))
 		self.g_b1 = np.zeros(784).astype(np.float32)
 
-		# init discriminator weights 
+		# init discriminator weights
 		self.d_W0 = np.random.randn(784,128).astype(np.float32) * np.sqrt(2.0/(784))
 		self.d_b0 = np.zeros(128).astype(np.float32)
 
@@ -43,7 +43,7 @@ class GAN(object):
 
 		self.d_h0_l = self.d_input.dot(self.d_W0) + self.d_b0
 		self.d_h0_a = lrelu(self.d_h0_l)
-		
+
 		self.d_h1_l = self.d_h0_a.dot(self.d_W1) + self.d_b1
 		self.d_h1_a = sigmoid(self.d_h1_l)
 		self.d_out = self.d_h1_a
@@ -53,7 +53,7 @@ class GAN(object):
 	def generator(self, z):
 		#self.g_h{num}_l : hidden logit layer
 		#self.g_h{num}_a : hidden activation layer
-		
+
 		self.z = np.reshape(z, (self.batch_size, -1))
 
 		self.g_h0_l = self.z.dot(self.g_W0) + self.g_b0
@@ -63,19 +63,19 @@ class GAN(object):
 		self.g_h1_a = tanh(self.g_h1_l)
 
 		self.g_out = np.reshape(self.g_h1_a, (self.batch_size, 28, 28))
-		
+
 		return self.g_h1_l, self.g_out
 
 
 	# generator backpropagation
-	def backprop_gen(self, 
-					 fake_logit, 
-					 fake_output, 
-					 fake_input, 
+	def backprop_gen(self,
+					 fake_logit,
+					 fake_output,
+					 fake_input,
 					 ):
 		# fake_logit : logit output from the discriminator D(G(z))
 		# fake_output : sigmoid output from the discriminator D(G(z))
-		
+
 		# flatten fake image input
 		fake_input = np.reshape(fake_input, (self.batch_size,-1))
 
@@ -88,7 +88,7 @@ class GAN(object):
 		loss_deriv = g_loss*sigmoid(fake_logit, derivative=True)
 		loss_deriv = loss_deriv.dot(self.d_W1.T)
 
-		loss_deriv = loss_deriv*lrelu(self.d_h0_l, derivative=True)		
+		loss_deriv = loss_deriv*lrelu(self.d_h0_l, derivative=True)
 		loss_deriv = loss_deriv.dot(self.d_W0.T)
 		# Reached the end of the generator
 
@@ -110,7 +110,7 @@ class GAN(object):
 		grad_W0 = np.matmul(prev_layer,loss_deriv_)
 		grad_b0 = loss_deriv
 
-		# calculated all the gradients in the batch 
+		# calculated all the gradients in the batch
 		# now make updates
 		for idx in range(self.batch_size):
 			self.g_W0 = self.g_W0 - self.learning_rate*grad_W0[idx]
@@ -121,21 +121,21 @@ class GAN(object):
 
 
 	# discriminator backpropagation
-	def backprop_dis(self, 
-					 real_logit, real_output, real_input, 
+	def backprop_dis(self,
+					 real_logit, real_output, real_input,
 					 fake_logit, fake_output, fake_input,
 					 ):
 		# real_logit : real logit value before sigmoid activation function (real input)
 		# real_output : Discriminator output in range 0~1 (real input)
 		# real_input : real input image fed into the discriminator
 		# fake_logit : fake logit value before sigmoid activation function (generated input)
-		# fake_output : Discriminator output in range 0~1 (generated input) 
+		# fake_output : Discriminator output in range 0~1 (generated input)
 		# fake_input : fake input image fed into the discriminator
 
 		real_input = np.reshape(real_input, (self.batch_size,-1))
 		fake_input = np.reshape(fake_input, (self.batch_size,-1))
 
-		# Discriminator loss = -np.mean(log(D(x)) + log(1-D(G(z)))) 
+		# Discriminator loss = -np.mean(log(D(x)) + log(1-D(G(z))))
 		# Calculate gradients of the loss
 		d_real_loss = -1.0/(real_output + epsilon)
 		d_fake_loss = -1.0/(fake_output - 1.0 + epsilon)
@@ -150,14 +150,14 @@ class GAN(object):
 		loss_deriv_ = np.expand_dims(loss_deriv, axis=1)
 		grad_real_W1 =  np.matmul(prev_layer,loss_deriv_)
 		grad_real_b1 = loss_deriv
-		
+
 		loss_deriv = loss_deriv.dot(self.d_W1.T)
 		loss_deriv = loss_deriv*lrelu(self.d_h0_l, derivative=True)
 		prev_layer = np.expand_dims(real_input, axis=-1)
 		loss_deriv_ = np.expand_dims(loss_deriv, axis=1)
 		grad_real_W0 = np.matmul(prev_layer,loss_deriv_)
-		grad_real_b0 = loss_deriv 
-		
+		grad_real_b0 = loss_deriv
+
 		#######################################
 		#		fake input gradients
 		#		-log(1 - D(G(z)))
@@ -167,7 +167,7 @@ class GAN(object):
 		loss_deriv_ = np.expand_dims(loss_deriv, axis=1)
 		grad_fake_W1 = np.matmul(prev_layer,loss_deriv_)
 		grad_fake_b1 = loss_deriv
-			
+
 		loss_deriv = loss_deriv.dot(self.d_W1.T)
 		loss_deriv = loss_deriv*lrelu(self.d_h0_l, derivative=True)
 		prev_layer = np.expand_dims(fake_input, axis=-1)
@@ -182,7 +182,7 @@ class GAN(object):
 
 		grad_W0 = grad_real_W0 + grad_fake_W0
 		grad_b0 = grad_real_b0 + grad_fake_b0
-	
+
 		# calculated all the gradients in the batch
 		# now make updates
 		for idx in range(self.batch_size):
@@ -205,7 +205,7 @@ class GAN(object):
 			for idx in range(batch_idx):
 				# prepare batch and input vector z
 				train_batch = trainX[idx*self.batch_size:idx*self.batch_size + self.batch_size]
-				#ignore batch if there are insufficient elements 
+				#ignore batch if there are insufficient elements
 				if train_batch.shape[0] != self.batch_size:
 					break
 
@@ -223,8 +223,8 @@ class GAN(object):
 				# add epsilon to avoid overflow
 				# maximize Discriminator loss = -np.mean(log(D(x)) + log(1-D(G(z))))
 				d_loss = -np.log(d_real_output+epsilon) - np.log(1 - d_fake_output+epsilon)
-				
-				# Generator loss 
+
+				# Generator loss
 				# ver1 : minimize log(1 - D(G(z)))
 				# ver2 : maximize -log(D(G(z))) #this is better
 				g_loss = -np.log(d_fake_output+epsilon)
@@ -235,12 +235,12 @@ class GAN(object):
 				################################
 				# for every result in the batch
 				# calculate gradient and update the weights using Adam
-							
-				# discriminator backward pass	
+
+				# discriminator backward pass
 				# one for fake input, another for real input
 				self.backprop_dis(d_real_logits, d_real_output, train_batch, d_fake_logits, d_fake_output, fake_img)
-				
-				# generator backward pass 
+
+				# generator backward pass
 				self.backprop_gen(d_fake_logits, d_fake_output, fake_img)
 				# train generator twice?
 				#g_logits, fake_img = self.generator(z)
@@ -252,7 +252,8 @@ class GAN(object):
 				img_tile(np.array(fake_img), self.img_path, epoch, idx, "res", False)
 				self.img = fake_img
 
-				print "Epoch [%d] Step [%d] G Loss:%.4f D Loss:%.4f Real Ave.: %.4f Fake Ave.: %.4f lr: %.4f"%(epoch, idx, np.mean(g_loss), np.mean(d_loss), np.mean(d_real_output), np.mean(d_fake_output), self.learning_rate)
+
+				print("Epoch [%d] Step [%d] G Loss:%.4f D Loss:%.4f Real Ave.: %.4f Fake Ave.: %.4f lr: %.4f"%(epoch, idx, np.mean(g_loss), np.mean(d_loss), np.mean(d_real_output), np.mean(d_fake_output), self.learning_rate))
 
 			#update learning rate every epoch
 			self.learning_rate = self.learning_rate * (1.0/(1.0 + self.decay*epoch))
@@ -267,7 +268,7 @@ class GAN(object):
 #program starts here
 #select numbers to generate
 #recommended to put a single number to get faster result
-numbers = [1]
+numbers = [2]
 
 gan = GAN(numbers)
-gan.train()	
+gan.train()
